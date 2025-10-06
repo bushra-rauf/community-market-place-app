c8f48a14c6ed0c8d09595baf7f5054d53c13fadd
bushra-rauf <bush_rauf@yahoo.com>
2025-10-06 22:55:52 +0200

    made query client provider and put it in layout file

---
+++ src/providers/query-client-provider.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { QueryClient, QueryClientProvider as OriginalQueryClientProvider } from "@tanstack/react-query"
+
+
+const makeQueryClient = () => {return new QueryClient}
+let browserQueryClient: QueryClient | undefined = undefined
+export const getQueryClient = () => {
+    if(typeof window === 'undefined') {
+        return makeQueryClient()
+    } else {
+        if (!browserQueryClient) {
+            browserQueryClient = makeQueryClient()
+        }
+        return browserQueryClient
+    }
+}
+
+export const QueryClientProvider = ({children}:{children: React.ReactNode}) => {
+    const QueryClient = getQueryClient()
+
+    return(
+        <OriginalQueryClientProvider client = {QueryClient}> 
+            {children}
+        </OriginalQueryClientProvider>
+    )
+}
