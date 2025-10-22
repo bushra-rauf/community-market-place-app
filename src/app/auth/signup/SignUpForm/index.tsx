5c786630fc49477d84916065e65a484171acef64
bushra-rauf <bush_rauf@yahoo.com>
2025-10-22 16:48:09 +0200

    added signup form

---
+++ src/app/auth/signup/SignUpForm/index.tsx
@@ -0,0 +1,24 @@
+const SignUpForm = () => {
+    return (
+        <>
+        <form className="flex flex-col mb-4">
+           <h2 className="mb-6">Sign Up!</h2>
+           <fieldset>
+               <label className="font-bold text-2xl p-2" htmlFor="email">Enter your email</label>
+               <input className="ml-2 mb-6 px-2" id="email" name="email" placeholder="Enter your email"/>
+           </fieldset>
+           <fieldset>
+               <label className="font-bold text-2xl p-2" htmlFor="Username">Enter your name</label>
+               <input className='ml-6 mb-5 px-2'id="username" name="username" placeholder="what is your name..."/>
+           </fieldset>
+            <fieldset>
+               <label className="font-bold text-2xl p-2" htmlFor="password">Enter your password</label>
+               <input className='ml-6 mb-5 px-2'id="password" name="password" placeholder="Enter your password"/>
+           </fieldset>
+           <button className=" bg-blue-400 hover:bg-blue-500 text-white p-4 font-bold text-2xl rounded-2xl">Sign Up</button>
+        </form>
+        </>
+    )
+}
+
+export default SignUpForm
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,14 @@
+import Link from "next/link"
+import SignUpForm from "./SignUpForm"
+
 const SignUp = () => {
     return(
-       <div>signup page</div>
+        <div className="mt-20 border-1 rounded-2xl w-140 h-85 mx-auto p-4">
+            <SignUpForm/>
+            <p className="pl-2">
+               Already have an account ? log in  <Link className='text-red-500' href='/auth/login'>here!</Link>
+            </p>
+        </div>
     )
 }
 export default SignUp
