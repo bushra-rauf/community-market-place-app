701dd7c11405db93eb65e803a27920ade0b0ebfb
bushra-rauf <bush_rauf@yahoo.com>
2025-10-26 23:46:42 +0100

    added errormessage component and update it in loginform

--- src/app/auth/login/LogInform/index.tsx
+++ src/app/auth/login/LogInform/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { LogIn } from "@/actions/log-in"
 import { logInSchema } from "@/actions/schemas"
+import ErrorMessage from "@/components/ErrorMessage"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
@@ -17,13 +18,13 @@ const LogInForm = () => {
            <fieldset>
                <label className="font-bold text-2xl p-2" htmlFor="email">Enter your email</label>
                <input className="ml-2 mb-6 px-2"{...register('email')} id="email" placeholder="Enter your email"/>
-               {errors.email && <p>{errors.email.message}</p>}
+               {errors.email && <ErrorMessage message= {errors.email.message!}/>}
            </fieldset>
             <fieldset>
                <label className="font-bold text-2xl p-2" htmlFor="password">Enter your password</label>
                <input className='ml-6 mb-5 px-2'{...register('password')}id="password" placeholder="Enter your password"/>
-           </fieldset>
-           <button className=" bg-blue-400 hover:bg-blue-500 text-white p-2 font-bold text-2xl rounded-xl">Log in</button>
+               {errors.password && <ErrorMessage message= {errors.password.message!}/>}
+            </fieldset><button className=" bg-blue-400 hover:bg-blue-500 text-white p-2 font-bold text-2xl rounded-xl">Log in</button>
         </form>
         </>
     )
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import LogInForm from "./LogInform"
 const LogIn = () => {
     return (
       <>
-        <div className="mt-20 border-1 rounded-2xl w-140 h-75 mx-auto p-4">
+        <div className="mt-20 border-1 rounded-2xl w-140 h-80 mx-auto p-4">
              <LogInForm/>
             Dont have an account ? sign up <Link className='text-red-500' href='/auth/signup'>here!</Link>
         </div>
