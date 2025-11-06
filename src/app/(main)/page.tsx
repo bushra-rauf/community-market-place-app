3d956ef63a6067c70c9e38ed44c538976a0e475e
bushra-rauf <bush_rauf@yahoo.com>
2025-11-06 21:45:19 +0100

    import cache  move post rendering to Home page and add delete with revalidation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,8 @@
 import HomePosts from "@/components/Home/HomePosts";
 import { getHomePosts } from "@/utils/supabase/quries";
 import { createClient } from "@/utils/supabase/server-client";
-
+import cache from "next/cache"
+import Link from "next/link";
   export const revalidate = 600;
 
 export default async function Home() {
@@ -14,7 +15,14 @@ export default async function Home() {
     return (
     <div className="w-[80%] mx-auto">
       {/* {data && data.map(({id, title, slug, users}) => <div key= {id}>{title}{users.username})} */}
-        <HomePosts posts={data!}/>
+        {/* <HomePosts posts={data!}/> */}
+            {data && data.map(({id, title, slug, users}) => 
+                <Link href={`/${slug}`}className='block border-1 p-4 rounded mt-4 max-w-2xl m-auto'key={id}>
+                   <h2 className="font-bold text-xl">{title}</h2> 
+                   <div className="text-right">by {users.username}</div>
+                </Link>
+            )}
+
     </div>
   )
 }
