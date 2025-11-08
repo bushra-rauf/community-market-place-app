eb674ef7dc36dab9551c6feb103a99eef171ad72
bushra-rauf <bush_rauf@yahoo.com>
2025-11-08 14:54:23 +0100

    added edit Button

---
+++ src/app/(main)/[slug]/EditButton/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from "next/link"
+
+const EditButton = ({slug}: {slug: string}) => {
+
+    
+    return(
+            <Link className="bg-black border p-2 text-white font-bold rounded-2xl" href={`/${slug}/edit`}>Edit post</Link>
+    )
+}
+
+export default EditButton
